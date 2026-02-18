75aa883893ae5e303e8a6a675458092e4efcad54
reinaldocarvalho201810-boop <reinaldocarvalho201810@gmail.com>
2026-02-18 14:04:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -138,7 +138,8 @@ app.get("/user/profile", async (req, res) => {
    
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
