80512e3ff04b7cf81e35fa21bb9f0e1808ac06db
reinaldocarvalho201810-boop <reinaldocarvalho201810@gmail.com>
2026-02-21 05:48:16 -0400

    Update wallet.routes.js

--- src/routes/wallet.routes.js
+++ src/routes/wallet.routes.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const { getBalance } = require('../modules/wallet/wallet.controller')
 const auth = require('../middleware/auth')
 
-router.get('/wallet/balance', auth, getBalance)
+router.get('/balance', auth, getBalance)
 
 module.exports = router
